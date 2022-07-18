5bb74d6ce5738c68073143a6ec4abcd4aef4cc16
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-07-18 18:06:09 +0300

    Add animal data to table

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals(name, date_of_birth, escape_attemps, neutered, weight_kg) values  ('Agumon', '2020-02-03', 0,true, 10.23);
+insert into animals(name, date_of_birth, escape_attemps, neutered, weight_kg) values ('Gabumon', '2018-11-15', 2, true, 8);
+insert into animals(name, date_of_birth, escape_attemps, neutered, weight_kg) values ('Pikachu', '2021-01-07', 1, false, 15.04);
+insert into animals(name, date_of_birth, escape_attemps, neutered, weight_kg) values ('Devimon', '2017-05-12', 5, true, 11) ;

931faf9bb284620155ccd18ea62fdf7962b5f05e
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-07-18 17:18:42 +0300

    Add animal coloumns

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+# CREATE TABLE animals(
+id integer,
+name char(100),
+date_of_birth date,
+escape_attemps integer,
+neutered boolean,
+weight_kg decimal
 );
